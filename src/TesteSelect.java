d9815b28980ba50d4caa564c69a98e4118067af8
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2022-09-09 19:28:29 -0300

    comittei

--- src/TesteSelect.java
+++ src/TesteSelect.java
@@ -5,7 +5,7 @@ import java.sql.Statement;
 
 public class TesteSelect {
     public static void main(String[] args) throws SQLException {
-        int id = 8;
+        int id = 2;
         String sql = "select * from contatos where id =" + id;
 
         Conexao conexao = new Conexao();
